9ee82c75d4e12e899ae7cb723b34af00ec00ff57
peter <pkarella27@yahoo.com>
2017-07-05 20:39:13 -0700

    refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+    protected
 
-      protected
-
-      def configure_permitted_parameters
-          devise_parameter_sanitizer.for(:sign_up) { |u| u.permit( :email, :password) }
-          devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :current_password, :avatar) }
-      end
-  end
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :username, :password) }
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :username, :password, :current_password, :avatar) }
+    end
+end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 
 class WelcomeController < ApplicationController
   def index
-  
   end
+
+    def user_params
+      params.require(:user).permit(:username, :email, :avatar)
+    end
 end
